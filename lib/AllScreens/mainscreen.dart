878f31c7ba70f4efede5ba881c6b21aefd1202cf
Clifford Jr. Mthemba <clifford@Cliffords-MacBook-Pro.local>
2021-07-29 16:42:00 +0400

    add reverse geocoding

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -5,9 +5,9 @@ import 'package:flutter/painting.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:uber/AllWidgets/Divider.dart';
+import 'package:uber/Assistants/assistantMethod.dart';
 
-class MainScreen extends StatefulWidget
-{
+class MainScreen extends StatefulWidget {
   static const String idScreen = "mainScreen";
 
   @override
@@ -15,25 +15,29 @@ class MainScreen extends StatefulWidget
 }
 
 class _MainScreenState extends State<MainScreen> {
-
   Completer<GoogleMapController> _controllerGoogleMap = Completer();
-   late GoogleMapController newGoogleMapController;
+  late GoogleMapController newGoogleMapController;
 
   GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
 
   late Position currentPosition;
   var geoLocator = Geolocator();
-  double bottomPaddingOfMap = 0 ;
+  double bottomPaddingOfMap = 0;
 
-  void locatePosition() async
-  {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  void locatePosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
     currentPosition = position;
-    
-    LatLng latLongPosition =LatLng(position.latitude, position.longitude);
-    
-    CameraPosition cameraPosition = new CameraPosition(target: latLongPosition, zoom: 14);
-    newGoogleMapController.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    LatLng latLongPosition = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition =
+        new CameraPosition(target: latLongPosition, zoom: 14);
+    newGoogleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    String address = await AssistantMethods.searchCoordinateAddress(position);
+    print("This is your address :: " + address);
   }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
@@ -57,18 +61,28 @@ class _MainScreenState extends State<MainScreen> {
               Container(
                 height: 165.0,
                 child: DrawerHeader(
-                  decoration:  BoxDecoration(
-                    color: Colors.white
-                  ),
+                  decoration: BoxDecoration(color: Colors.white),
                   child: Row(
                     children: [
-                      Image.asset("images/user_icon.png", height: 65.0, width: 65.0,),
-                      SizedBox(width: 16.0,),
+                      Image.asset(
+                        "images/user_icon.png",
+                        height: 65.0,
+                        width: 65.0,
+                      ),
+                      SizedBox(
+                        width: 16.0,
+                      ),
                       Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Profile name", style: TextStyle(fontSize: 16.0, fontFamily: "Brand-Bold"),),
-                          SizedBox(height: 6.0,),
+                          Text(
+                            "Profile name",
+                            style: TextStyle(
+                                fontSize: 16.0, fontFamily: "Brand-Bold"),
+                          ),
+                          SizedBox(
+                            height: 6.0,
+                          ),
                           Text("Visit Profile"),
                         ],
                       )
@@ -77,48 +91,56 @@ class _MainScreenState extends State<MainScreen> {
                 ),
               ),
               DividerWidget(),
-              SizedBox(height: 12.0,),
+              SizedBox(
+                height: 12.0,
+              ),
 
               //drawer body buttons
               ListTile(
                 leading: Icon(Icons.history),
-                title: Text("History", style: TextStyle(fontSize: 15.0),),
+                title: Text(
+                  "History",
+                  style: TextStyle(fontSize: 15.0),
+                ),
               ),
               ListTile(
                 leading: Icon(Icons.person),
-                title: Text("Profile", style: TextStyle(fontSize: 15.0),),
+                title: Text(
+                  "Profile",
+                  style: TextStyle(fontSize: 15.0),
+                ),
               ),
               ListTile(
                 leading: Icon(Icons.info),
-                title: Text("About", style: TextStyle(fontSize: 15.0),),
+                title: Text(
+                  "About",
+                  style: TextStyle(fontSize: 15.0),
+                ),
               ),
             ],
           ),
         ),
       ),
-
-
       body: Stack(
         children: [
           GoogleMap(
-             padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
-             mapType: MapType.normal,
-             myLocationButtonEnabled: true,
-             initialCameraPosition: _kGooglePlex,
-             myLocationEnabled: true,
-             zoomGesturesEnabled: true,
-             zoomControlsEnabled: true,
-             onMapCreated: (GoogleMapController controller)
-             {
-               _controllerGoogleMap.complete();
-                newGoogleMapController = controller;
+            padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            myLocationEnabled: true,
+            zoomGesturesEnabled: true,
+            zoomControlsEnabled: true,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete();
+              newGoogleMapController = controller;
 
-                setState(() {
-                  bottomPaddingOfMap = 265.0;
-                });
+              setState(() {
+                bottomPaddingOfMap = 265.0;
+              });
 
-               locatePosition();
-             },
+              locatePosition();
+            },
           ),
 
           //HamburgerButton for drawer
@@ -126,28 +148,26 @@ class _MainScreenState extends State<MainScreen> {
             top: 45.0,
             left: 22.0,
             child: GestureDetector(
-              onTap: (){
+              onTap: () {
                 scaffoldKey.currentState!.openDrawer();
               },
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(22.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 6.0,
-                      spreadRadius: 0.5,
-                      offset: Offset(
-                        0.7,
-                        0.7
-                      )
-                    )
-                  ]
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(22.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 6.0,
+                          spreadRadius: 0.5,
+                          offset: Offset(0.7, 0.7))
+                    ]),
                 child: CircleAvatar(
                   backgroundColor: Colors.white,
-                  child: Icon(Icons.menu, color: Colors.black,),
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  ),
                   radius: 20.0,
                 ),
               ),
@@ -161,79 +181,117 @@ class _MainScreenState extends State<MainScreen> {
               child: Container(
                 height: 300.0,
                 decoration: BoxDecoration(
-                  color:  Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 16.0,
-                      spreadRadius: 0.5,
-                      offset: Offset(0.7, 0.7)
-                    )
-                  ]
-            ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(18.0),
+                        topRight: Radius.circular(18.0)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 16.0,
+                          spreadRadius: 0.5,
+                          offset: Offset(0.7, 0.7))
+                    ]),
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 24.0, vertical: 18.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(height: 6.0,),
-                      Text("Hi there, ", style: TextStyle(fontSize: 12.0),),
-                      Text("Where to, ", style: TextStyle(fontSize: 20.0, fontFamily: "Brand-Bold"),),
-                      SizedBox(height: 20.0,),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Text(
+                        "Hi there, ",
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                      Text(
+                        "Where to, ",
+                        style:
+                            TextStyle(fontSize: 20.0, fontFamily: "Brand-Bold"),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
                       Container(
                         decoration: BoxDecoration(
-                            color:  Colors.white,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(5.0),
                             boxShadow: [
                               BoxShadow(
                                   color: Colors.black54,
                                   blurRadius: 6.0,
                                   spreadRadius: 0.5,
-                                  offset: Offset(0.7, 0.7)
-                              )
-                            ]
-                        ),
+                                  offset: Offset(0.7, 0.7))
+                            ]),
                         child: Padding(
                           padding: const EdgeInsets.all(12.0),
                           child: Row(
                             children: [
-                              Icon(Icons.search, color: Colors.blueAccent,),
-                              SizedBox(width: 10.0,),
+                              Icon(
+                                Icons.search,
+                                color: Colors.blueAccent,
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
                               Text("Search Drop Off")
                             ],
                           ),
                         ),
                       ),
-                      SizedBox(height: 24.0,),
+                      SizedBox(
+                        height: 24.0,
+                      ),
                       Row(
                         children: [
-                          Icon(Icons.home, color: Colors.grey,),
-                          SizedBox( width: 12.0,),
+                          Icon(
+                            Icons.home,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text("Add Home"),
-                              SizedBox(height: 4.0,),
-                              Text("Your living home address", style: TextStyle(color: Colors.black54, fontSize: 12.0)),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text("Your living home address",
+                                  style: TextStyle(
+                                      color: Colors.black54, fontSize: 12.0)),
                             ],
                           )
                         ],
                       ),
-
-                      SizedBox(height: 10.0,),
+                      SizedBox(
+                        height: 10.0,
+                      ),
                       DividerWidget(),
-                      SizedBox(height: 16.0,),
+                      SizedBox(
+                        height: 16.0,
+                      ),
                       Row(
                         children: [
-                          Icon(Icons.work, color: Colors.grey,),
-                          SizedBox( width: 12.0,),
+                          Icon(
+                            Icons.work,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text("Add Work"),
-                              SizedBox(height: 4.0,),
-                              Text("Your Office address", style: TextStyle(color: Colors.black54, fontSize: 12.0)),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text("Your Office address",
+                                  style: TextStyle(
+                                      color: Colors.black54, fontSize: 12.0)),
                             ],
                           )
                         ],
@@ -241,7 +299,7 @@ class _MainScreenState extends State<MainScreen> {
                     ],
                   ),
                 ),
-          ))
+              ))
         ],
       ),
     );
